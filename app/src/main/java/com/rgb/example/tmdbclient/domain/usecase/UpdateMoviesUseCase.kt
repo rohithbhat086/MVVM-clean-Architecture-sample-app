8404494b99035abb42278efa8f522cf84b7d062b
rohithbhat086 <80900670+rohithbhat086@users.noreply.github.com>
2021-05-16 10:44:51 +0530

    temp changes

--- app/src/main/java/com/rgb/example/tmdbclient/domain/usecase/UpdateMoviesUseCase.kt
+++ app/src/main/java/com/rgb/example/tmdbclient/domain/usecase/UpdateMoviesUseCase.kt
@@ -3,7 +3,9 @@ package com.rgb.example.tmdbclient.domain.usecase
 import com.rgb.example.tmdbclient.data.model.Movie
 import com.rgb.example.tmdbclient.domain.repository.MovieRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class UpdateMoviesUseCase @Inject constructor(private val repository: MovieRepository) {
 
     suspend fun execute(): List<Movie> {
